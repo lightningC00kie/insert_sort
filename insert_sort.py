a7124474f24b7a322c6d4b8722ea1329c33fcdcc
lightningC00kie <kareemhamed24@gmail.com>
2021-07-11 01:13:29 +0200

    added descending order sort branch

--- insert_sort.py
+++ insert_sort.py
@@ -1,8 +1,16 @@
-A = [9,12,4,1,3,15]
+i=0
+A = []
+num=0
+while num != 'x':
+    num = input('input number\n')
+    if num == 'x':
+        break
+    A.append(num)
+    i=i+1
 for j in range(1, len(A)):
     key = A[j]
     i=j-1
-    while i>-1 and A[i] > key:
+    while i>-1 and A[i] < key:
         A[i+1] = A[i]
         i=i-1
     A[i+1] = key
